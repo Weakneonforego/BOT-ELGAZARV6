21494b5de1648a62383c03e71a89ce292faa599d
Juhabach <125589655+Weakneonforego@users.noreply.github.com>
2023-05-07 18:53:05 +0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,22 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
+global.owner = ['971544517448'] //ur owner number
+global.ownername = "يوهاباخ ساما" //ur owner name
+global.ytname = "•☾︎𝐖𝐚𝐧𝐝𝐞𝐧𝐫𝐞𝐢𝐜𝐡 𓆩♧︎︎︎𓆪  𝐊𝐢𝐧𝐠𝐝𝐨𝐦 👑•" //ur yt chanel name
+global.socialm = "•☾︎𝐖𝐚𝐧𝐝𝐞𝐧𝐫𝐞𝐢𝐜𝐡 𓆩♧︎︎︎𓆪  𝐊𝐢𝐧𝐠𝐝𝐨𝐦 👑•" //ur github or insta name
+global.location = "•☾︎𝐖𝐚𝐧𝐝𝐞𝐧𝐫𝐞𝐢𝐜𝐡 𓆩♧︎︎︎𓆪  𝐊𝐢𝐧𝐠𝐝𝐨𝐦 👑•" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
-global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.ownernomer = "971 54 451 7448" //ur number
+global.premium = ['971 54 451 7448] //ur premium number
+global.botname = 'Aluka' //ur bot name
+global.linkz = "https://chat.whatsapp.com/Kn0jMoltbRW8qsRp5WRd1E" //your theme url which will be displayed on whatsapp
+global.websitex = "https://chat.whatsapp.com/Kn0jMoltbRW8qsRp5WRd1E" //ur website to be ' //script link
+global.themeemoji = "♣️" //ur theme emoji
+global.packname = "•☾︎𝐖𝐚𝐧𝐝𝐞𝐧𝐫𝐞𝐢𝐜𝐡 𓆩♧︎︎︎𓆪  𝐊𝐢𝐧𝐠𝐝𝐨𝐦 👑•" //ur sticker watermark packname
+global.author = "⍣•☾︎𝐖𝐚𝐧𝐝𝐞𝐧𝐫𝐞𝐢𝐜𝐡 𓆩♧︎︎︎𓆪  𝐊𝐢𝐧𝐠𝐝𝐨𝐦 👑•⍣" //ur sticker watermark author
+global.wm = "•☾︎𝐖𝐚𝐧𝐝𝐞𝐧𝐫𝐞𝐢𝐜𝐡 𓆩♧︎︎︎𓆪  𝐊𝐢𝐧𝐠𝐝𝐨𝐦 👑•." //ur watermark
 
 // Other
 global.sessionName = 'session'
@@ -40,16 +39,16 @@ global.sp = ''
 
 //messages
 global.mess = {
-    success: 'تم يروحي✨',
-    admin: '『 الامر دا لادمن بس! 』',
+    success: ' حاضر سيدي✨',
+    admin: '『الامر هذا للأخيار و بس 』',
     botAdmin: '『 ارفع البوت ادمن الاول 』',
     premime: '『 ميزات خاصة متميزة إذا كنت ترغب في تسجيل نوع الإيجار 』',
-    owner: '『 الميزه دي للمطور بس! 』',
+    owner: '『 الميزه ذي للملك بس! 』',
     group: '『 الميزه دي للجروبات بس! 』',
     private: '『 الميزه دي في الخاص بس! 』',
     bot: '『 الميزه دي للبوت بس! 』',
-    wait: '『 استني شويه... 』',
-    linkm: '『 فين الرابط? 』',
+    wait: '『جاري العمله... 』',
+    linkm: '『 اين الرابط? 』',
     endLimit: 'انتهت صلاحية الحد اليومي الخاص بك ، وسيتم إعادة تعيين الحد كل 12 ساعة',
     nsfw: 'لم يتم تنشيط ميزة nsfw ، يرجى الاتصال بالمسؤول للتفعيل',
 }
